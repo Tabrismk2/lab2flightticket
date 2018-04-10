29be19763571a8808399eaef302aedb16aead5d6
Tabrismk2 <tabrisyuan@Yes-MacBook-Pro.local>
2018-04-09 18:32:13 -0700

    add apis, reservation api need some work

--- src/main/java/edu/sjsu/cmpe275/lab2/MainController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/MainController.java
@@ -3,9 +3,11 @@ package edu.sjsu.cmpe275.lab2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.text.NumberFormat;
 import java.util.*;
 import java.sql.Date;
 
@@ -31,7 +33,7 @@ public class MainController {
     @Autowired
     private FlightReservationRepository flightReservationRepository;
 
-    @RequestMapping(path="/passenger", method = RequestMethod.POST ) // Create Passenger API
+    @RequestMapping(path="/passenger", method = RequestMethod.POST ) // CreatePassenger API
     public @ResponseBody ResponseEntity addNewPassenger (@RequestParam String firstname,
                                                  @RequestParam String lastname,
                                                  @RequestParam int age,
@@ -56,31 +58,87 @@ public class MainController {
         }
     }
 
-    @RequestMapping(path="/reservation", method = RequestMethod.POST) // Create Reservation API
-    public @ResponseBody String addNewReservation(@RequestParam String id){
-        Reservation reservation = new Reservation();
-        Passenger passenger = passengerRepository.findById(id).get();
-        reservation.setPassenger(passenger);
-        //reservation.setFlights(List<flight>);
-        reservationRepository.save(reservation);
-        int i = 0;
-        do{
-            FlightReservation flightReservation = new FlightReservation();
-            flightReservation.setFlightNumber(String.valueOf(i));
-            flightReservation.setReservationNumber(reservation.getReservationNumber());
-            flightReservationRepository.save(flightReservation);
-            FlightPassenger flightPassenger = new FlightPassenger();
-            flightPassenger.setFlightNumber(String.valueOf(i));
-            flightPassenger.setPassengerID(id);
-            flightPassengerRepository.save(flightPassenger);
-        }
-        while(++i < 2);
+    @RequestMapping(path = "/passenger/{id}", method = RequestMethod.GET) //GetPassengerById API
+    public @ResponseBody ResponseEntity getPassengerById(@PathVariable("id") String passengerId) {
+        Optional<Passenger> find_result = passengerRepository.findById(passengerId);
 
-        return "Saved";
+        try {
+            Passenger passenger = find_result.get();
+            return new ResponseEntity<Passenger>(passenger, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "Passenger id cannot be found");
+            return new ResponseEntity<Object>(apiError, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @RequestMapping(path="/flight/flightnumber", method = RequestMethod.POST)
-    public @ResponseBody String addNewFlight(@RequestParam double price,
+    @RequestMapping(value = "passenger/{id}", method = RequestMethod.PUT)//UpdatePassenger API
+    public @ResponseBody ResponseEntity updatePassenger (@PathVariable("id") String passengerId,
+                                                         @RequestParam String firstname,
+                                                         @RequestParam String lastname,
+                                                         @RequestParam int age,
+                                                         @RequestParam String gender,
+                                                         @RequestParam String phone){
+        Optional<Passenger> find_result = passengerRepository.findById(passengerId);
+        try{
+
+                Passenger passenger = find_result.get();
+                passenger.setFirstname(firstname);
+                passenger.setLastname(lastname);
+                passenger.setAge(age);
+                passenger.setGender(gender);
+                passenger.setPhone(phone);
+
+                passengerRepository.save(passenger);
+                return new ResponseEntity<Passenger>(passenger, HttpStatus.OK);
+        } catch(NumberFormatException e){
+                ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "WRONG Parameter Type");
+                return new ResponseEntity<Object>(apiError, HttpStatus.BAD_REQUEST);
+            } catch(NoSuchElementException e){
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "Passenger with id XXX does not exist");
+            return new ResponseEntity<Object>(apiError, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "passenger/{id}", method = RequestMethod.DELETE)//Delete Passenger
+    public @ResponseBody ResponseEntity deletePassenger (@PathVariable("id") String passengerId) {
+        Optional<Passenger> find_result = passengerRepository.findById(passengerId);
+
+        try {
+            Passenger passenger = find_result.get();
+            passengerRepository.delete(passenger);
+            return new ResponseEntity<Object>("Passenger with id XXX is deleted successfully", HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "Passenger with id XXX does not exist");
+            return new ResponseEntity<Object>(apiError, HttpStatus.NOT_FOUND);
+        }
+    }
+
+//    @RequestMapping(path="/reservation", method = RequestMethod.POST) // Create Reservation API
+//    public @ResponseBody ResponseEntity addNewReservation(@RequestParam String passengerId
+//                                                          @RequestParam("flightLists") List<Flight> flightLists){
+//        Reservation reservation = new Reservation();
+//        Passenger passenger = passengerRepository.findById(id).get();
+//        reservation.setPassenger(passenger);
+//        //reservation.setFlights(List<flight>);
+//        reservationRepository.save(reservation);
+//        int i = 0;
+//        do{
+//            FlightReservation flightReservation = new FlightReservation();
+//            flightReservation.setFlightNumber(String.valueOf(i));
+//            flightReservation.setReservationNumber(reservation.getReservationNumber());
+//            flightReservationRepository.save(flightReservation);
+//            FlightPassenger flightPassenger = new FlightPassenger();
+//            flightPassenger.setFlightNumber(String.valueOf(i));
+//            flightPassenger.setPassengerID(id);
+//            flightPassengerRepository.save(flightPassenger);
+//        }
+//        while(++i < 2);
+//
+//        return "Saved";
+//    }
+
+    @RequestMapping(path="/flight/flightnumber", method = RequestMethod.POST) //Create/UpdateFlight API
+    public @ResponseBody ResponseEntity addNewFlight(@RequestParam double price,
                                              @RequestParam String origin,
                                              @RequestParam String to,
                                              @RequestParam String description,
@@ -105,44 +163,45 @@ public class MainController {
         plane.setModel(model);
         plane.setYear(year);
         flight.setPlane(plane);
-        flightRepository.save(flight);
-        return "Saved";
-    }
+        try{
+            flightRepository.save(flight);
+            return new ResponseEntity<Flight>(flight, HttpStatus.OK);
+        }catch(NumberFormatException e){
+            ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "Error 400");
+            return new ResponseEntity<Object>(apiError, HttpStatus.BAD_REQUEST);
 
-    @GetMapping(path = "/passenger/all")
-    public @ResponseBody Iterable<Passenger> getAllPassenger(){
-        return passengerRepository.findAll();
-    }
-
-    @RequestMapping(path = "/passenger/{id}", method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity<Passenger> findById(@PathVariable("id") String passengerId) {
-        Passenger find_result = passengerRepository.findByPassengerId(passengerId);
-
-        try {
-            return new ResponseEntity<Passenger>(find_result,HttpStatus.OK);
-        } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("Passenger ID cannot be found.");
-        }
-    }
-
-    @GetMapping(path = "/reservation/{reservationNumber}")
-    public @ResponseBody Optional<Reservation> getReservation(@RequestParam String reservationNumber){
-        return reservationRepository.findById(reservationNumber);
-    }
-
-    @RequestMapping(value = "passenger/{id}", method = RequestMethod.DELETE)//Delete Passenger
-    public @ResponseBody ResponseEntity deletePassenger (@PathVariable("id") String passengerId) {
-        Optional<Passenger> find_result = passengerRepository.findById(passengerId);
-
-        try {
-            Passenger passenger = find_result.get();
-            passengerRepository.delete(passenger);
-            return new ResponseEntity<Object>("Passenger with id XXX is deleted successfully", HttpStatus.OK);
-        } catch (NoSuchElementException e) {
-            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "Passenger with id XXX does not exist");
+        }catch(NoSuchElementException e){
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "Error 404 ");
             return new ResponseEntity<Object>(apiError, HttpStatus.NOT_FOUND);
         }
     }
+
+    @RequestMapping(path = "/flight/{flightNumber}", method = RequestMethod.GET)
+    public @ResponseBody ResponseEntity getFlightByFlightNumber(@PathVariable("flightNumber") String flightNumber){
+        Optional<Flight> find_result = flightRepository.findById(flightNumber);
+
+        try {
+            Flight flight = find_result.get();
+            return new ResponseEntity<Flight>(flight, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "Flight Number cannot be found");
+            return new ResponseEntity<Object>(apiError, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(path = "/flight/{flightNumber}", method = RequestMethod.DELETE)
+    public @ResponseBody ResponseEntity deleteFlightByFlightNumber(@PathVariable("flightNumber") String flightNumber){
+        Optional<Flight> find_result = flightRepository.findById(flightNumber);
+        try {
+            Flight flight = find_result.get();
+            flightRepository.delete(flight);
+            return new ResponseEntity<Object>("Flight with flightnumber XXX is deleted successfully", HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "Flight with id XXX does not exist");
+            return new ResponseEntity<Object>(apiError, HttpStatus.NOT_FOUND);
+        }
+    }
+
 
 
 }
